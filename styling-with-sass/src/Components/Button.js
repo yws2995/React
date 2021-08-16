bd4091ca2250f847b560f8ea0df980c4e7e6a72b
yws2995 <yws2995@naver.com>
2021-08-16 16:39:21 +0900

    styling-with-sass

--- styling-with-sass/src/Components/Button.js
+++ styling-with-sass/src/Components/Button.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import classNames from 'classnames';
 import './Button.scss';
 
-function Button({children}) {
-    return <button className = "Button">{children}</button>
-}
+function Button({children, size, color, outline, fullWidth, ...rest}) {
+    return <button {...rest} className = {classNames('Button', size, color, {outline,fullWidth})}>{children}</button>
+}   //className에 css 클래스 이름을 동적으로 넣어주기
+
+Button.defaultProps = {
+    size: 'medium',
+    color: 'blue'
+};
 
 export default Button;
